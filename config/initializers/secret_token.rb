95405d75bd2910f2233ec7ecb51925ba734c113a
Arnaud Lachaume <arnaud.lachaume@gmail.com>
2011-03-07 23:05:33 +1100

    Rails structure created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Switcher::Application.config.secret_token = 'da7c438b85c1431d576a288fde07e0d47d64aa66fc20b49b4ce3bff8a6f878b3a3e615e90fd1a03347ce233700774a83d954b0f3afdba39a20eb74e6ef24c08e'
